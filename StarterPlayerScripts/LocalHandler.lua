6c8de2a86db4f9f08cc20031eb16b25e4cafab06
DevComp <74418041+TheAwesomeCoder05@users.noreply.github.com>
2022-02-19 22:38:26 +0530

    finish localhandler script

--- StarterPlayerScripts/LocalHandler.lua
+++ StarterPlayerScripts/LocalHandler.lua
@@ -1,1 +1,8 @@
-local GunHandler = game.ReplicatedStorage:WaitForChild("Groza")
+local GunHandler = game.ReplicatedStorage:WaitForChild("Groza") --gun model
+local Viewmodel = game.ReplicatedStorage:WaitForChild("Viewmodel") --viewmodel arms
+
+
+--importing mainmodule
+local MainModule = require(game.ReplicatedStorage.MainModule)
+
+Viewmodel.Parent = game.Workspace.Camera
