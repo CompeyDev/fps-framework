35479a625696204e721cb00d3380c59ee8b0777f
DevComp <74418041+TheAwesomeCoder05@users.noreply.github.com>
2022-02-20 18:08:26 +0530

    make it so the gun can be shot and bullets are generated using `module.cast` for projectile prediction

--- StarterPlayerScripts/LocalHandler.lua
+++ StarterPlayerScripts/LocalHandler.lua
@@ -13,3 +13,33 @@ end)
 
 MainModule.weldgun(GunModel) --importing weldgun function
 MainModule.equip(Viewmodel, GunModel, AnimationsFolder.Hold) --call hold animation function with animation
+
+
+ game.GetService("UserInputService").InputBegan:Connect(function(input)
+      if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        isPlayerHoldingMouse = true --shoot on user input
+      end
+ end)
+  
+ game.GetService("UserInputService").InputEnded:Connect(function(input)
+      if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        isPlayerHoldingMouse = false --stop shooting when mouse released
+      end
+ end)
+
+ local IsPlayerHoldingMouse --make the gun shoot lol
+ local CanFire = true
+ local Delay = 0.1 
+  
+ game.GetService("RunService").Heartbeat:Connect(function(dt)
+      if IsPlayerHoldingMouse then
+        if CanFire then
+          CanFire = false
+          
+          MainModule.cast(GunModel, game.Players.LocalPlayer:GetMouse().Hit.Position, 60)
+          
+          wait(Delay)
+          CanFire = true
+        end
+      end
+ end)  
