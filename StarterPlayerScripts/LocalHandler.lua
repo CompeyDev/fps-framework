d47322d7fd597e9d111bf127b19a7ec58e0bfbf4
DevComp <74418041+TheAwesomeCoder05@users.noreply.github.com>
2022-02-20 11:33:40 +0530

    make localhandler use renderstepped to update every frame

--- StarterPlayerScripts/LocalHandler.lua
+++ StarterPlayerScripts/LocalHandler.lua
@@ -6,3 +6,7 @@ local Viewmodel = game.ReplicatedStorage:WaitForChild("Viewmodel") --viewmodel a
 local MainModule = require(game.ReplicatedStorage.MainModule)
 
 Viewmodel.Parent = game.Workspace.Camera
+
+game:GetService("RunService").RenderStepped:Connect(function(dt)
+    MainModule.update(Viewmodel, dt)
+end)
